3f5933ef301089511e7fc597ef6b9e5aae042819
mohit makda <mohitmakda1404@gmail.com>
2023-11-09 01:11:52 +0530

    teampics

---
+++ config/Team.js
@@ -0,0 +1,152 @@
+export const teamConfig = {
+    TeamMembers: [
+      {
+ 
+        teamName: "Tech team",
+        memberName: "Siddharth Sahu",
+        logo: "/images/CoreTeam/Siddharth - Siddharth Sahu.jpg",
+        link: {
+          twitter:"https://twitter.com/_SiddharthSahu",
+          Linkedin:"https://www.linkedin.com/in/siddharth-sahu-10/",
+        },
+      },
+      {
+
+        teamName: "Tech team",
+        memberName: "Mohit Makda",
+        logo: "/images/CoreTeam/mohit.jpg",
+        link: {
+          twitter:"",
+          Linkedin:"",
+        },
+      },
+      {
+ 
+        teamName: "Tech team",
+        memberName: "Harsh Gupta",
+        logo: "/images/CoreTeam/Harsh Gupta.jpg",
+        link: {
+          twitter:"",
+          Linkedin:"",
+          twitter:"https://x.com/harshguptatwt",
+          Linkedin:"https://www.linkedin.com/in/harshgupta-in",
+        },
+      },
+      {
+
+        teamName: "Graphics team",
+        memberName: "Gaurav Chandrakar",
+        logo: "/images/CoreTeam/gaurav.jpg",
+        link: {
+          twitter:"",
+          Linkedin:"",
+        },
+      },
+      {
+
+        teamName: "Graphics team",
+        memberName: "Shubham Jha",
+        logo: "/images/CoreTeam/Shubham Jha.jpg",
+        link: {
+          twitter:"",
+          Linkedin:"https://www.linkedin.com/in/shubham-jha-58b7b0281?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+      },
+      {
+        teamName: "Graphics team",
+        memberName: "Akshat Sahu",
+        logo: "/images/CoreTeam/Akshat Sahu.jpg",
+        link: {
+          twitter:"https://twitter.com/akshatx134",
+          Linkedin:"https://www.linkedin.com/in/akshat-sahu-912521216/",
+        },
+      },
+      {
+        teamName: "Graphics team",
+        memberName: "Smit Javia",
+        logo: "/images/CoreTeam/Smit Javia.jpg",
+        link: {
+          twitter:"https://twitter.com/BTW_IM_GAURAV",
+          Linkedin:"https://www.linkedin.com/in/gaurav-chandrakar-86431a226?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+      },
+      {
+        teamName: "Content team",
+        memberName: "Shweta C",
+        logo: "/images/CoreTeam/Shweta C.jpg",
+        link: {
+          twitter:"https://twitter.com/ShwetaC04389034",
+          Linkedin:"https://www.linkedin.com/in/shweta-cheemalapati-0082b8237/",
+        },
+      },
+      {
+        teamName: "Content team",
+        memberName: "Darshita Ahuja",
+        logo: "/images/CoreTeam/darshita.png",
+        link: {
+          twitter:"https://twitter.com/darshuahuja",
+          Linkedin:"https://www.linkedin.com/in/darshita-ahuja-9a448b225/",
+        },
+      },
+      {
+        teamName: "Content team",
+        memberName: "Lomesh Patil",
+        logo: "/images/CoreTeam/Lomesh Patil.jpg",
+        link: {
+          twitter:"https://x.com/lomeshhpatil?t=RdxSLtYEr0-ZXBRzD-Aqsg&s=09",
+          Linkedin:"https://www.linkedin.com/in/lomeshpatil?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+      },
+      {
+        teamName: "Pr team",
+        memberName: "Anubhav Choubey",
+        logo: "/images/CoreTeam/anubhav choubey.png",
+        link: {
+          twitter:"https://x.com/Anubhav0401?t=m1OJOt82TsR217geATvI6g&s=09",
+          Linkedin:"https://www.linkedin.com/in/anubhav-choubey-653680150?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+      },
+      {
+        teamName: "Pr team",
+        memberName: "Ashutosh Kashyap",
+        logo: "/images/CoreTeam/Ashutosh_Kashyap.jpg",
+        link: {
+          twitter:"https://x.com/Ashutosh_4523?t=WNdEZWq0KomclZ6LvPMmgA&s=09",
+          Linkedin:"https://www.linkedin.com/in/ashutosh-kashyap-8600a6240?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+      },
+      {
+        teamName: "Pr team",
+        memberName: "Prashansa Purohit",
+        logo: "/images/CoreTeam/Prashansa  - Prashansa Purohit.jpg",
+        link: {
+          twitter:"https://x.com/Prashansa_707_?t=qR8eg-me9N8vacj1lh2O4Q&s=09",
+          Linkedin:"https://www.linkedin.com/in/prashansa-purohit-a9a786239?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+      },
+      {
+        teamName: "Logistics Team",
+        memberName: "Anay Tripathy",
+        logo: "/images/CoreTeam/anay.jpeg",
+        link: {
+          twitter:"https://twitter.com/_am_Kittu_",
+          Linkedin:"https://www.linkedin.com/in/anaytripathy?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+        },
+      },
+      {
+        teamName: "Logistics Team",
+        memberName: "Apeksha Vaishnav",
+        logo: "/images/CoreTeam/apeksha.jpg",
+        link: {
+          twitter:"https://twitter.com/Apekshav12",
+          Linkedin:"https://www.linkedin.com/in/apeksha-vaishnaw-56533726a/",
+        },
+      },
+      
+      
+  
+      
+    ],
+
+  };
+  
