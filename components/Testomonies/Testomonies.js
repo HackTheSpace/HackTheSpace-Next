7c976d010661594e8a1b7d10678db2b5db0ff52e
mohit makda <mohitmakda1404@gmail.com>
2024-04-17 01:45:14 +0530

    squased commit
    testomonials
    
    testomonials
    
    testomonie
    
    testomonie-final

---
+++ components/Testomonies/Testomonies.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import SectionLayout from '../SectionLayout';
+import { TestomoniesConfig } from '../../config/Testomonies';
+import { TestomoniesConfig1 } from '../../config/Testomonies';
+import Marquee from 'react-fast-marquee';
+import Image from 'next/image';
+
+const Testomonies = () => {
+  return (
+    <SectionLayout Title={'TESTOMONIE'}  id={'Testomonie'}>
+
+
+        <Marquee speed={70} direction='right' pauseOnHover className='Marquee'>
+        <div className='Testomonies-main-div' style={{}}>
+          {/* Map over testimonies */}
+          {TestomoniesConfig.Details.map((item) => (
+            <div key={item.id} className='Testomonies-section'>
+              <p>{item.description}</p>
+              <div className='Testomonies-Info'>
+                <Image src={item.icon} alt='testimony-image' height={50} width={50} />
+                <div>
+                  <h4>{item.name}</h4>
+                  <span>{item.about}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        </Marquee>
+        <Marquee speed={70} style={{display:'flex'}} pauseOnHover>
+        <div className='Testomonies-main-div' style={{}}>
+          
+          {TestomoniesConfig1.Details.map((item) => (
+            // 
+            <div key={item.id} className='Testomonies-section'>
+              <p>{item.description}</p>
+              <div className='Testomonies-Info'>
+              <Image src={item.icon} alt='testimony-image' height={50} width={50} />
+                <div>
+                  <h4>{item.name}</h4>
+                  <span>{item.about}</span>
+                </div>
+              </div>
+            </div>
+           
+          ))}
+        </div>
+        </Marquee>
+    {/* </div> */}
+    </SectionLayout>
+    
+  );
+};
+
+export default Testomonies;
+
+
---
+++ config/Testomonies.js
@@ -0,0 +1,86 @@
+import { icon } from "@fortawesome/fontawesome-svg-core"
+
+
+export const TestomoniesConfig ={
+    Details:[
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+
+        ]
+}
+
+export const TestomoniesConfig1 ={
+    Details:[
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },  
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        {
+            name: "khushi trivedi",
+            about:"Speaker",
+            description:`Participating in this hackathon was an incredible experience! The organizers did a fantastic job of creating a collaborative environment where innovation thrived. I was amazed by the diversity of ideas and the level of talent displayed by the participants. Can't wait for the next one!`,
+            icon:"/images/Speakers/Khushi Trivedi.jpeg",
+
+            
+        },
+        
+    ]
+}
