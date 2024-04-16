1b69ac7c21ae3c4f868de5624e2fe8b8c2b44ff6
Harsh Gupta <harshgupta3341@gmail.com>
2024-04-17 02:07:36 +0530

    changed navbar design and changed style for headings

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,34 +7,18 @@ import ShootingStars from "../utils/ShootingStars.json";
 import Telescope from "../utils/Telescope.json";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-  const router = useRouter();
-  const handleClick = () => {
-    router.push("/Team");
-  };
-
-  const handleScroll = (e) => {
-    // first prevent the default behavior
-    e.preventDefault();
-    // get the href and remove everything before the hash (#)
-    setOpen(false);
-    //Y band krdega navbar navigation link click krne pr
-    const href = e.currentTarget.href;
-    const targetId = href.replace(/.*\#/, "");
-    // get the element by id and use scrollIntoView
-    const elem = document.getElementById(targetId);
-    elem?.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
-
   return (
     <nav>
       <div className="desktop-nav">
-        <div className="logo shrink">
-          <Link href="#home" onClick={handleScroll}>
-            <img src="/images/navbar/nav_logo.png" alt="hack the space" />
-          </Link>
+        <div className="left-heading nav-sponsor">
+          <div className="letter-container">
+            <p>CENTRAL </p>
+            <p>INDIA’S</p>
+          </div>
+          <div className="letter-container">
+            <p>BIGGEST </p>
+            <p>HACKATHON</p>
+          </div>
         </div>
 
         <div className="nav-sponsor">
--- components/Sponsor/SponsorContainer.js
+++ components/Sponsor/SponsorContainer.js
@@ -3,31 +3,6 @@ import Image from "next/image";
 const SponsorContainer = ({ Name, Img, Type, children, wid }) => {
   return (
     <div className={`sponsor ${Name}`}>
-      <div className="Sponsor-name-container">
-        <hr className={`left-hr ${wid}`} />
-        <Image
-          src={Img}
-          alt={Name}
-          style={
-            Name === "inkind" ||
-            Name === "hiringPartner" ||
-            Name === "mediaPartner" ||
-            Name === "communityPartner"
-              ? { opacity: "0" }
-              : {}
-          }
-          width={100}
-          height={Name === "gold" ? 130 : 100}
-        />
-        <h2
-          className={`sponsor-name name-${Type} ${
-            Name === "inkind" ? "name-inkind" : ""
-          }`}
-        >
-          {Type}
-        </h2>
-        <hr className={`right-hr ${wid}`} />
-      </div>
       <div className="wrapper">{children}</div>
     </div>
   );
